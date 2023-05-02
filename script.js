5c869920df1a30d7b71642a8405ad7e3fbedf333
Lívio Gabriel <liviogabriel98@gmail.com>
2023-05-02 12:06:49 -0300

    Correção Realizada

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Tabuleiro inicial de exemplo
-const seaBoard = [
-  [".", ".", ".", ".", "."],
+const seaBoard = [  [".", ".", ".", ".", "."],
   [".", ".", ".", ".", "."],
   [".", ".", ".", ".", "."],
   [".", ".", ".", ".", "."],
@@ -14,9 +13,9 @@ const seaBoard = [
 // Função para alocar os navios no tabuleiro
 function allocateShips(shipPositions, initialBoard) {
   for (let i = 0; i < shipPositions.length; i++) {
-    let row = shipPositions[i][0];
-    let col = shipPositions[i][1];
-    initialBoard[row][col] = "S";
+    let linha = shipPositions[i][0];
+    let coluna = shipPositions[i][1];
+    initialBoard[linha][coluna] = "S";
   }
   return initialBoard;
 }
@@ -24,10 +23,10 @@ function allocateShips(shipPositions, initialBoard) {
 // Função para verificar as suposições do jogador
 function checkGuesses(guesses, mountedBoard) {
   for (let i = 0; i < guesses.length; i++) {
-    let row = guesses[i][0];
-    let col = guesses[i][1];
-    if (mountedBoard[row][col] === "S") {
-      mountedBoard[row][col] = "X";
+    let linha = guesses[i][0];
+    let coluna = guesses[i][1];
+    if (mountedBoard[linha][coluna] === "S") {
+      mountedBoard[linha][coluna] = "X";
     }
   }
   return mountedBoard;
